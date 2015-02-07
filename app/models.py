488d6902e6ecf17951fd340b7b7db808b580aaa7
Shoubik Mani <shouvikm@andrew.cmu.edu>
2015-02-07 11:42:58 -0500

    User Model

--- app/models.py
+++ app/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+#User model represents user accounts
+class User(models.Model):
+	firstName = models.CharField(max_length = 50)
+	lastName = models.CharField(max_length = 50)
+	username = models.CharField(max_length = 50)
+	hashedPassword = models.CharField(max_length = 50)
+	phoneNumber = models.CharField(max_length = 50)
+	email = models.CharField(max_length = 50)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+	#Represents User as a string when it is called
+	def __str__(self):
+		return "%s, %s, %s" % (self.firstName_text, self.lastName_text, self.username_text)
 
+	#Orders the Users in the database by first name
+	class Meta:
+		ordering = ('firstName_text',)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
