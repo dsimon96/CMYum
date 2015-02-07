ef4c4c045c377262bce374d48e7de6d0e559d40d
cvsmith <cvsmith@andrew.cmu.edu>
2015-02-07 14:15:41 -0500

    Made order form work, linked to it from indx, may or may not add orders to db

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,7 @@ from app import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name = "index"),
     url(r'^(?P<blankErrorStatus>\w+)/createAccount/$', views.createAccount, name = 'createAccount'),
+    url(r'^(?P<blankErrorStatus>\w+)/createOrder/$', views.createOrder, name = 'createOrder'),
+    url(r'^addOrder/$', views.addOrder, name = 'addOrder'),
     url(r'^addUser/$', views.addUser, name = 'addUser'),
 )
